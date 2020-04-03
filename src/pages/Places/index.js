011dbbf0037be8c4b9079edc8233b8c51a8a0178
Gabriel Cunha <gn_cunha@hotmail.com>
2020-04-02 22:48:32 -0300

    consumindo Rest api/renderizando marcações no mapa

--- src/pages/Places/index.js
+++ src/pages/Places/index.js
@@ -1,34 +1,70 @@
 import React from 'react'
-import {View,Text,StyleSheet,Dimensions} from 'react-native'
-import MapView from 'react-native-maps'
+import {View,Text,StyleSheet,Dimensions,TouchableOpacity} from 'react-native'
+import MapView,{Marker} from 'react-native-maps'
+import api from '../../Services/Places.api'
+
 
 export default class Places extends React.Component {
-    render() {
-      return (
-        <View style={styles.container}>
-          <MapView style={styles.mapStyle} 
-           initialRegion={{
-               latitude:-1.4347828,
-               longitude:-48.4515453,
-               latitudeDelta: 0.0080,
-               longitudeDelta: 0.0060 
-           }}
-          />
-        </View>
-      );
+     state={
+      places:null,
+      coords:{
+        latitude:null,
+        longitude:null,
+      },
+      loading:true
+       }      
+      searchPlaces = ()=>{
+         api.get('mapbox.places/hospital.json?bbox=-48.468709,-1.451940,-48.436370,-1.419611&access_token=pk.eyJ1IjoiZ2Ficml4ZGQiLCJhIjoiY2s4amM2N3phMDIzbzNlcWJ3aG9wZnFiOSJ9.ZgSDvbJErpkkhQeYi3i-5w')
+         .then(response =>{
+          
+          this.setState({
+             places:response.data.features,
+             loading:false
+           })
+         })
+       }
+
+       componentDidMount(){
+         this.searchPlaces()
+       }
+  render() {
+    const{places,loading,coords} = this.state
+    if(loading){
+     return <Text>Teste</Text>
     }
+    return (
+      <View style={styles.container}>
+        <MapView style={styles.mapStyle} 
+         initialRegion={{
+             latitude:-1.4347828,
+             longitude:-48.4515453,
+             latitudeDelta: 0.0080,
+             longitudeDelta: 0.0060 
+         }}
+        >
+        {places.map((location,id)=><Marker
+         key={id}
+         coordinate={{
+           latitude:location.geometry.coordinates[1],
+           longitude:location.geometry.coordinates[0],
+         }}
+         title={location.text}
+        />)}
+        </MapView>
+      </View>
+    );
   }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    mapStyle: {
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height,
-    },
-  });
-  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width:400,
+    height:400,
+  },
+});
